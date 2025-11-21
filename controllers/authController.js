11f4aa4cbbd4b3f710ae8bed856e9b9c4a23449e
boomstickz <ali.alam@ontariotechu.net>
2025-11-21 17:44:58 -0500

    Implement full authentication logic (register, login, logout)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,42 @@
-module.exports = {
-  loginPage: (req, res) => res.render("login"),
-  registerPage: (req, res) => res.render("register"),
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
-  loginUser: (req, res) => {
-    // TODO: authentication logic
-    res.redirect("/dashboards");
-  },
+module.exports.registerPage = (req, res) => {
+  res.render("register");
+};
 
-  registerUser: (req, res) => {
-    // TODO: hashing + create user
-    res.redirect("/auth/login");
-  },
+module.exports.registerUser = async (req, res) => {
+  const { username, password } = req.body;
 
-  logoutUser: (req, res) => {
-    req.session.destroy(() => res.redirect("/"));
-  }
+  const hashed = await bcrypt.hash(password, 12);
+
+  await User.create({
+    username,
+    password: hashed
+  });
+
+  res.redirect("/auth/login");
+};
+
+module.exports.loginPage = (req, res) => {
+  res.render("login");
+};
+
+module.exports.loginUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+  if (!user) return res.redirect("/auth/login");
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) return res.redirect("/auth/login");
+
+  req.session.userId = user._id;
+  res.redirect("/dashboards");
+};
+
+module.exports.logoutUser = (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
 };
