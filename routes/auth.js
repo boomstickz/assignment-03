1f485a461a66a3371066bc623fb802cf3ec47e6d
boomstickz <ali.alam@ontariotechu.net>
2025-11-29 22:49:23 -0500

    Added GitHub OAuth routes and disabled unused Google auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -4,9 +4,7 @@ const authController = require("../controllers/authController");
 
 // Register + Login Pages
 router.get("/login", authController.loginPage);
-router.get("/register", (req, res) => {
-  res.render("register", { error: null });
-});
+router.get("/register", authController.registerPage);
 
 
 // Form Submissions
@@ -14,5 +12,9 @@ router.post("/login", authController.loginUser);
 router.post("/register", authController.registerUser);
 
 router.get("/logout", authController.logoutUser);
+
+// GitHub OAuth
+router.get("/github", authController.githubLogin);
+router.get("/github/callback", authController.githubCallback);
 
 module.exports = router;
